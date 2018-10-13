3330fd4f4c8b273a3ebad0a695c37d8174946d04
maquetSDU <43057203+maquetSDU@users.noreply.github.com>
2018-10-13 14:00:29 +0600

    Add files via upload

---
+++ week #5 Tokanov Miras/task2/script2.js
@@ -0,0 +1,8 @@
+let images = document.getElementsByTagName("img");
+for(let i = 0; i < images.length; i++ ){
+    images[i].addEventListener('click',makeBig);
+}
+function makeBig(event){
+    let src = event.currentTarget.getAttribute("src");
+    document.getElementsByTagName("img")[0].setAttribute("src",src);
+}
